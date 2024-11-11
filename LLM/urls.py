3b19648bc87be80f27fffd2321db12b9ca51a84f
islam302 <islambadran39@gmail.com>
2024-11-11 05:23:58 +0200

    p

--- LLM/urls.py
+++ LLM/urls.py
@@ -3,5 +3,5 @@ from .views import OpenaiAPIView, NewsDetailAPIView
 
 urlpatterns = [
     path('create/', OpenaiAPIView.as_view(), name='create_news'),
-    path('create/<int:pk>/', NewsDetailAPIView.as_view(), name='news_detail'),
+    path('news/<int:pk>/', NewsDetailAPIView.as_view(), name='news_detail'),
 ]
