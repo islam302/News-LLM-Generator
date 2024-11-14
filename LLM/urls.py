4c3b812a4788a7e6d58e68e03545647b0c0d4607
islam302 <islambadran39@gmail.com>
2024-11-14 02:04:14 +0200

    link with frontend

--- LLM/urls.py
+++ LLM/urls.py
@@ -4,4 +4,5 @@ from .views import OpenaiAPIView, NewsDetailAPIView
 urlpatterns = [
     path('create/', OpenaiAPIView.as_view(), name='create_news'),
     path('news/<int:pk>/', NewsDetailAPIView.as_view(), name='news_detail'),
+
 ]
